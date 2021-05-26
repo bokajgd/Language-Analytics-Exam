e227cb585382634efa798bbf44cd413c73a038af
Jakob Grøhn <bokajgd@gmail.com>
2021-05-26 22:09:23 +0200

    All code works

--- A2-Collocation/src/A2-Collocation.py
+++ A2-Collocation/src/A2-Collocation.py
@@ -1,17 +1,20 @@
-# Importing packages
-import os
+#-----# Importing packages #-----#
+
+import argparse
 import re
 
 from pathlib import Path
 import pandas as pd
 import numpy as np
 
-# Basic python scripting using object-oriented coding
+#-----# Project desctiption #-----#
+
+
+# Basic python scripting using object-oriented coding - word collocation
 '''
 Using the corpus called 100-english-novels, write a Python programme which does the following:
 
 - The script should take a directory of text files, a keyword, and a window size (number of words) as input parameters, and an output file called out/{filename}.csv
-These parameters can be defined in the script itself
 - Find out how often each word collocates with the target across the corpus
 - Use this to calculate mutual information between the target word and all collocates across the corpus
 - Save result as a single file consisting of three columns: collocate, raw_frequency, MI
@@ -20,50 +23,76 @@ These parameters can be defined in the script itself
 '''
 Shortcomings of this script:
 - The current function concatenates all text files before calculating relevant scores. Therefore, the 'window' slides across the boundaries of each text which is not optimal.
-- What if a collocate appears multiple times in a given window? 
 '''
 
-# Defining main function 
-def main():
-    Collocation(keyword = "cat", window_size = 2) # Change argument input to generate new files with different keywords or window sizes
+#-----# Defining main function #-----#
+
+def main(args):
+    
+    # Initialising arguments that can be specified in command line
+    keyword = args.key
+    
+    window_size = args.ws
+
+    Collocation(keyword=keyword, window_size=window_size)
 
 # Setting class 'CountFunctions'
 class Collocation:
 
 
     def __init__(self, keyword, window_size):
-        self.keyword = keyword # Defining keyword as the parsed argument 'keyword'
-        self.window_size = window_size # Defining keyword as the parsed argument 'window_size'
 
-        data_dir = self.setting_data_directory() # Setting data directory 
-        out_dir = self.setting_output_directory() # Setting output directory for the generated csv file
-        files = self.get_paths_from_data_directory(data_dir) # Getting list of filepaths for the images
+        # Defining keyword as the parsed argument 'keyword' as a self-variable
+        self.keyword = keyword 
 
-        tokenized_text = self.get_tokenized_concatenated_texts(files) # Getting tokenized version off concatenated text corpus
+        # Defining keyword as the parsed argument 'window_size' as a self-variable
+        self.window_size = window_size 
 
-        collocates, R1 = self.get_list_of_collocates(tokenized_text, self.keyword , self.window_size)  # Getting list of unique collocates
+        # Setting data directory 
+        self.data_dir = self.setting_data_directory() 
+        
+        # Setting output directory for the generated csv file
+        self.out_dir = self.setting_output_directory() 
 
-        raw_frequencies = [] # Empty list for raw frequencies of collocates
- 
-        MIs = [] # Empty list for MI-scores between keyword and collocate for all collocates
+        # Getting list of filepaths for the images
+        files = self.get_paths_from_data_directory(self.data_dir) 
 
-        # Loop through collocate
+        # Getting tokenized version off concatenated text corpus
+        tokenized_text = self.get_tokenized_concatenated_texts(files) 
+
+        # Getting list of unique collocatess
+        collocates, R1 = self.get_list_of_collocates(tokenized_text, self.keyword , self.window_size)  
+
+        # Empty list for raw frequencies of collocates
+        raw_frequencies = [] 
+        
+        # Empty list for MI-scores between keyword and collocate for all collocates
+        MIs = [] 
+
+        # Loop through collocates
         for collocate in collocates: 
+            
+            # Raw frequency of collocate
+            collocate_raw_frequency = self.get_raw_frequency(tokenized_text, collocate)  
 
-            collocate_raw_frequency = self.get_raw_frequency(tokenized_text, collocate)  # Raw frequency of collocate
+            # Joint frequency of keyword and collocate
+            O11 = self.get_O11(tokenized_text, self.keyword, collocate, self.window_size)  
 
-            O11 = self.get_O11(tokenized_text, self.keyword , collocate, self.window_size)  # Joint frequency of keyword and collocate
+            # Calculating C1: Same as raw frequency of collocate
+            C1 = collocate_raw_frequency  
 
-            C1 = collocate_raw_frequency  # Calculating: Same as raw frequency of collocate
+            # O11 + O12 + O21 + O22
+            N = len(tokenized_text)  
 
-            N = len(tokenized_text)  # O11 + O12 + O21 + O22
+            # Expected frequencys
+            E11 = (R1 * C1 / N)  
+            
+            # Calculating Mutual information
+            MI = np.log2(O11 / E11)  
 
-            E11 = (R1 * C1 / N)  # Expected frequency
-
-            MI = np.log2(O11 / E11)  # Mutual information
-
-            # Adding information for given collocate to list
+            # Adding information for given collocate to lists
             raw_frequencies.append(collocate_raw_frequency) 
+
             MIs.append(MI)
 
         # Gathering needed lists into a dictionary
@@ -73,12 +102,15 @@ class Collocation:
 
         # Creating pd data frame from dictionary
         df = pd.DataFrame(data=data_dict)
+        
+        # Sorting collocates with highest frequency at the top.
+        df = df.sort_values("MI", ascending=False)  
 
-        df = df.sort_values("MI", ascending=False)  # Sorting collocates with highest frequency at the top.
+        # Path for csv file with unique name
+        write_path = self.out_dir / f"{self.keyword}_collocates_ws_{self.window_size}.csv" 
 
-        write_path = out_dir / f"{self.keyword}_collocates_ws_{self.window_size}.csv" # Path for csv file 
-
-        df.to_csv(write_path) # Writing csv files
+        # Writing csv files
+        df.to_csv(write_path) 
 
 
 
@@ -111,11 +143,13 @@ class Collocation:
         files (list): List of individual file paths
     '''
     def get_paths_from_data_directory(self, data_dir):
-
-        files = [] # Creating empty list
+        
+        # Creating empty list
+        files = [] 
 
         # Loop for iterating through all files in the directory and append individual file paths to the empty list files
         for file in data_dir.glob('*.txt'): 
+
             files.append(file)
 
         return files
@@ -131,13 +165,16 @@ class Collocation:
         window_size (integer): Specified window size in which to search for collocates
     Returns:
         unique_collocates (list): List of unique collocates
+        R1 (int) : Number of possible pairs with key (number of collocates)
     '''
     def get_list_of_collocates(self, tokenized_text, keyword, window_size):
 
-            collocates = [] # Create empty list for all collocate
+            # Create empty list for all collocate
+            collocates = [] #
 
             for i, word in enumerate(tokenized_text):
-
+                
+                # If keyword is detected, generate a window around it and extract collocates (words in the window)
                 if word == keyword:
 
                     left_window = tokenized_text[max(0, i - window_size):i]
@@ -146,10 +183,14 @@ class Collocation:
 
                     total_window = left_window + right_window
 
-                    collocates.extend(total_window) # Add all collocates from window to the full list of collocates
+                    # Add all collocates from window to the full list of collocates
+                    collocates.extend(total_window) 
+            
+            # Only keep one of each unique collocate
+            unique_collocates = pd.unique(collocates)
 
-            unique_collocates = pd.unique(collocates) # Only keep one of each unique collocate
-            R1 = len(collocates) # Number of all collocates in each window combined
+            # Number of all collocates in each window combined 
+            R1 = len(collocates) 
 
             return unique_collocates, R1
 
@@ -163,9 +204,11 @@ class Collocation:
         word_frequency (integer): Count of word frequency
     '''
     def get_raw_frequency(self, tokenized_text, focus_word):
-
+        
+        # Start counter at zero
         word_frequency = 0
 
+        # Loop through all tokenised words and count number of times word is found
         for word in tokenized_text:
 
             if word == focus_word:
@@ -188,21 +231,26 @@ class Collocation:
     '''
     def get_O11(self, tokenized_text, keyword, collocate, window_size):
 
+        # Stat counter at zero
         O11 = 0
 
+        # Loop through tokens
         for i, word in enumerate(tokenized_text):
 
-            if word == keyword: # Record all instances of keyword and create a window around it
+                # Record all instances of keyword and create a window around it
+                if word == keyword: 
+                    
+                    # Cannot go beyond first word
+                    left_window = tokenized_text[max(0, i - window_size):i]
 
-                left_window = tokenized_text[max(0, i - window_size):i]
+                    right_window = tokenized_text[i+1:(i + window_size)]
 
-                right_window = tokenized_text[i:(i + window_size + 1)]
+                    total_window = left_window + right_window
 
-                total_window = left_window + right_window
+                    # Count how many times (if any) collocate is in list
+                    collocate_count = total_window.count(collocate)
 
-                if keyword and collocate in total_window: # If collocate is in the window then add one to count
-
-                    O11 += 1
+                    O11 += collocate_count
 
         return O11
 
@@ -216,12 +264,14 @@ class Collocation:
        text (string): Text of a txt file
     '''
     def load_text(self, file):
+
         # Read each .txt file
         with open(file, encoding="utf-8") as file:
 
             text = file.read()
             
-            text = text.lower() # Using lowercase method to set all characters to lowercase
+            # Using lowercase method to set all characters to lowercase
+            text = text.lower() 
 
             file.close()
 
@@ -237,33 +287,39 @@ class Collocation:
         tokenized_text (list): List of individual tokens
     '''
     def tokenize(self, input_text):
+
         # Split on any non-alphanumeric character
         tokenizer = re.compile(r"\W+")
 
         # Tokenize
         tokenized_text = tokenizer.split(input_text) 
 
-        # Return token_list
+        # Return token list
         return tokenized_text
 
 
     # Defining function for concatenating .txt files into one text    
     '''
-    Creates a combined text from multiple files
+    Creates a combined tokenised text from multiple files
     Args:
-        files (list): List of file paths
+        files (list) : List of file paths
     Returns:
-        text_corpus (string): Full tokenised text
+        text_corpus (string) : Full tokenised text
     '''
     def get_tokenized_concatenated_texts(self, files):
+
+        # Empty list for full tokenized text corpus
         tokenized_text_corpus = []
 
         for file in files:
-
+            
+            # Load text
             text = self.load_text(file)
 
+            # Use tokenizer to toxenize text
             tokenized_text = self.tokenize(text)
 
+            # Append tokenized text to full corpus list
             tokenized_text_corpus.extend(tokenized_text)
 
         return tokenized_text_corpus
@@ -271,4 +327,33 @@ class Collocation:
 
 # Executing main function when script is run
 if __name__ == '__main__':
-    main()
+
+    #Create an argument parser from argparse
+    parser = argparse.ArgumentParser(description = "[INFO] Calculating Word Collocation Mutual Information Scores",
+                                formatter_class = argparse.RawTextHelpFormatter)
+
+    parser.add_argument('-key', 
+                        metavar="--keyword",
+                        type=str,
+                        help=
+                        "[DESCRIPTION] The name of the desired keyword\n"
+                        "[TYPE]        str \n"
+                        "[DEFAULT]     cat \n"
+                        "[EXAMPLE]     -key cat \n",
+                        required=False,
+                        default='cat')
+
+
+    parser.add_argument('-ws', 
+                        metavar="--window_size",
+                        type=int,
+                        help=
+                        "[DESCRIPTION] How many words the sliding window \n"
+                        "              should extend on both sides of the keywod \n"
+                        "[TYPE]        int \n"
+                        "[DEFAULT]     2 \n"
+                        "[EXAMPLE]     -ws 2 \n",
+                        required=False,
+                        default=2)
+        
+    main(parser.parse_args())
