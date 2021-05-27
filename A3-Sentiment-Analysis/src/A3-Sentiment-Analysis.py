c570f21aacf9a7731b2e542be29c370e0fe6b81e
Jakob Grøhn <bokajgd@gmail.com>
2021-05-27 05:45:37 +0200

    Code changed to produce rolling averages, new plots, new subset

--- A3-Sentiment-Analysis/src/A3-Sentiment-Analysis.py
+++ A3-Sentiment-Analysis/src/A3-Sentiment-Analysis.py
@@ -1,5 +1,5 @@
-# Importing packages
-import os
+#-----# Importing packages #-----#
+
 import argparse
 from pathlib import Path
 import pandas as pd
@@ -7,50 +7,92 @@ import matplotlib.pyplot as plt
 import spacy
 from spacytextblob.spacytextblob import SpacyTextBlob
 
+#-----# Project desctiption #-----#
+
 # Basic python scripting using object-oriented coding
 '''
-Calculate the sentiment score for every headline in the data. You can do this using the spaCyTextBlob approach that we covered in class or any other dictionary-based approach in Python.
+Calculate the sentiment score for every headline in the data. 
 Create and save a plot of sentiment over time with a 1-week rolling average
 Create and save a plot of sentiment over time with a 1-month rolling average
 Make sure that you have clear values on the x-axis and that you include the following: a plot title; labels for the x and y axes; and a legend for the plot
-Write a short summary (no more than a paragraph) describing what the two plots show. You should mention the following points: 1) What (if any) are the general trends? 2) What (if any) inferences might you draw from them?
+Write a short summary (no more than a paragraph) describing what the two plots show. You should mention the following points: 
+1) What (if any) are the general trends? 2) What (if any) inferences might you draw from them?
 '''
+
+#-----# Defining main function #-----#
 
 # Defining main function 
 def main(args):
-    filename = args.filename # Adding argument that can be specified in command line
 
-    Sentiment(filename = filename) # Calling main class 
+    # Adding argument that can be specified in command line
+    filename = args.fn 
 
-# Setting class 'CountFunctions'
-class Sentiment:
+    data_dir = args.dd
 
-    def __init__(self, filename):
+    # Calling main class 
+    SentimentAnalysis(filename=filename, data_dir=data_dir) 
 
-        data_dir = self.setting_data_directory() # Setting directory of input data 
-        out_dir = self.setting_output_directory() # Setting directory of output plots
 
-        self.filename = filename # Setting filename as the provided filename
+#-----# Defining class #-----#
 
-        if self.filename is None: # If no filename is specified, use test_data.csv as default file
-            self.filename = "test_data.csv"
+class SentimentAnalysis:
 
-        df = pd.read_csv(data_dir / f'{self.filename}')  # Read csv file
-        df["polarity"] = self.get_polarity_score(text = df["headline_text"]) # Adding a column with calculated polarity scores for each headline
+    def __init__(self, filename, data_dir):
+        
+        # Setting directory of input data
+        if data_dir is None:
 
-        df_grouped_daily = df.groupby('publish_date', as_index=False)['polarity'].mean() # Grouping headlines and calculating a mean score for each day
+            self.data_dir = self.setting_data_directory() 
+         
+        else:
 
-        self.create_plot(out_dir = out_dir, data = df_grouped_daily['polarity'].rolling(7).mean(), plot_name = "Weekly_Rolling_Average_Polarity") # Generating and saving plot of 7-day rolling average sentiment score
+            self.data_dir = data_dir
 
-        self.create_plot(out_dir = out_dir, data = df_grouped_daily['polarity'].rolling(30).mean(), plot_name = "Monthly_Rolling_Average_Polarity") # Generating and saving plot of 30-day rolling average sentiment score
+        # Setting directory of output plots 
+        self.out_dir = self.setting_output_directory()
 
+        # Setting filename as the provided filename
+        self.filename = filename 
+
+        # If no filename is not specified, use test_data.csv as default file
+        if self.filename is None: 
+
+            self.filename = "abcnews-date-text_subset.csv"
+
+        # Read csv file
+        df = pd.read_csv(self.data_dir / f'{self.filename}')  
+        
+        # Adding a column with calculated polarity scores for each headline
+        df["polarity"] = self.get_polarity_score(text = df["headline_text"]) 
+
+        # Converting publish data columnt to datetime
+        df['publish_date'] =  pd.to_datetime(df['publish_date'], format='%Y%m%d')
+
+        # Deleting column with text
+        del df['headline_text']
+        
+        # Grouping by different periods 
+        df_grouped_daily = self.group_data(df, 'd')
+
+        self.df_grouped_yearly = self.group_data(df, 'y')
+
+        # Generating and saving plot of 7-day rolling average sentiment score
+        self.create_plot(data = df_grouped_daily, rolling = 7, plot_name = "Weekly_Rolling_Average_Polarity") 
+
+        # Generating and saving plot of 30-day rolling average sentiment score
+        self.create_plot(data = df_grouped_daily, rolling = 30, plot_name = "Monthly_Rolling_Average_Polarity") 
+
+
+    #-#-# UTILITY FUNCTIONS #-#-#  
 
     # Defining function for setting directory for the raw data
     def setting_data_directory(self):
-
-        root_dir = Path.cwd()  # Setting root directory
-
-        data_dir = root_dir / 'data'   # Setting data directory
+        
+        # Setting root directory
+        root_dir = Path.cwd()  
+        
+        # Setting data directory
+        data_dir = root_dir / 'data'
 
         return data_dir
 
@@ -58,9 +100,11 @@ class Sentiment:
     # Defining function for setting directory for the output
     def setting_output_directory(self):
 
-        root_dir = Path.cwd()  # Setting root directory
+        # Setting root directory
+        root_dir = Path.cwd()  
 
-        out_dir = root_dir / 'output' # Setting output directory
+        # Setting output directory
+        out_dir = root_dir / 'output' 
 
         return out_dir
     
@@ -72,53 +116,117 @@ class Sentiment:
         text: Input text
     Returns:
         polarity (list): List containing polarity scores for each headline
-
     '''
     def get_polarity_score(self, text):
 
-        nlp = spacy.load("en_core_web_sm") # Initialise spaCy
+        # Loading spaCy model
+        nlp = spacy.load("en_core_web_sm") 
 
-        spacy_text_blob = SpacyTextBlob() # Initialise spaCyTextBlob 
+        # Initialise spaCyTextBlob 
+        spacy_text_blob = SpacyTextBlob()   
 
-        nlp.add_pipe(spacy_text_blob) # Add spaCyTextBlob as a new component to our spaCy nlp pipeline
+        # Add spaCyTextBlob as a new component to our spaCy nlp pipeline  
+        nlp.add_pipe(spacy_text_blob) 
 
-        polarity = [] # Creating empy list for 
+        # Creating empy list for scores
+        polarity = [] 
 
-        for headline in nlp.pipe(text): # Loop through each headline and calculate polarity score
+        # Loop through each headline and calculate polarity score
+        for headline in nlp.pipe(text): 
+
             polarity_score = headline._.sentiment.polarity 
-            polarity.append(float(polarity_score)) # Append score to list
+            
+            # Append score to list
+            polarity.append(float(polarity_score)) 
 
         return polarity
 
 
     # Defining function for creating a plot of development in sentiment scores 
-    def create_plot(self, out_dir, data, plot_name):
+    '''
+    Generates and save a matplotlib plot showing time series development in polarity scores
+    Args:
+        data (pd.df) : Input data frame of polarity scores grouped after according to time interval
+        rolling (iny) : How many days to calculate roling average over
+        plot_name (str) : Name of plot both for file name and title
+    '''
+    def create_plot(self, data, rolling, plot_name):
+        
+        # Plot data
+        fig, ax = plt.subplots(1, 1, dpi=300) 
 
-        plt.plot(data, label="Rolling Average Polarity", color = "#3E5B75") # Plot data
+        # Plot lines
+        ax.plot(data['polarity'].rolling(rolling).mean(), label="Rolling Average Polarity", color = "#3E5B75") 
 
-        plt.title(plot_name) # Define a title
+        ax.plot(self.df_grouped_yearly['polarity'], label="Yearly Average Polarity", color = "#E09D1E") 
 
-        plt.legend(loc='upper right') # Define legend position
+        # Define a title
+        plt.title(plot_name) 
 
-        plt.xlabel('Date') # Define x-axis label
+        # Define legend position
+        plt.legend(loc='upper right') 
 
-        plt.ylabel('Polarity') # Define y-axis label
+        # Define x-axis label
+        plt.xlabel('Date')  
 
-        path = out_dir / f"{plot_name}.png" # Define output path using out_dir
+        # Define y-axis label
+        plt.ylabel('Polarity')
 
-        plt.savefig(path) # Save figure
+        # Rotate xticks
+        fig.autofmt_xdate()
+
+        # Define output path using out_dirs
+        path = self.out_dir / f"{plot_name}.png" 
+        
+        # Save figure
+        plt.savefig(path) 
 
         plt.close()
+
+
+    # Defining function for grouping data by period
+    '''
+    Returns a data frame that has been grouped by specified period
+    Args:
+        df (pd.df) : Input data with polarity scores and data columns
+        period (str) : Period on which to group by (e.g. 'w' for week)
+    Returns:
+        grouped_df (pd.df): Grouped data frame
+    '''
+    def group_data(self, df, period):
+        
+        # Group by period
+        df_grouped = df.groupby(df['publish_date'].dt.to_period(period)).mean()
+
+        # Return index to dates for xticks later 
+        df_grouped.index = df_grouped.index.to_timestamp()
+        
+        return df_grouped
+
 
 # Executing main function when script is run
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--filename', 
-                        metavar="Filename",
+    parser.add_argument('-fn', 
+                        metavar="--filename",
                         type=str,
-                        help='The name of the input data file',
+                        help=
+                        "[DESCRIPTION] The name of the input data file \n"
+                        "[TYPE]        str \n"
+                        "[DEFAULT]     abcnews-date-text_subset.csv \n"
+                        "[EXAMPLE]     -fn abcnews-date-text_subset.csv \n",
+                        required=False)
+    
+    parser.add_argument('-dd', 
+                        metavar="--data_directory",
+                        type=str,
+                        help=
+                        "[DESCRIPTION] Path to directory where data is located \n"
+                        "[TYPE]        str \n"
+                        "[DEFAULT]     A3-Sentiment-Analysis/data \n"
+                        "[EXAMPLE]     -dd A3-Sentiment-Analysis/data \n",
                         required=False)
 
     main(parser.parse_args())
